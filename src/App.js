a045ab2f19e329a360391f02a8ccf63fa876fb88
Austin Dang <91802108+AuDang@users.noreply.github.com>
2022-03-24 09:11:18 -0700

    Signs

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Navbar from "./components/Navbar";
 import SeparatorOne from "./components/SeparatorOne";
 import SideCard from "./components/SideCard";
 
-function App (){
+function App() {
   return (
     <>
       <Navbar />
--- src/context/HoroscopeContext.js
+++ src/context/HoroscopeContext.js
@@ -1,3 +1,15 @@
-import { createContext } from 'react';
+import { createContext, useState } from "react";
+import horoscopeObj from "../data/horoscopes";
 
 export const HoroscopeContext = createContext();
+
+const HoroscopeProvider = (props) => {
+  const [currentSign, setCurrentSign] = useState("Scorpio");
+  const sign = horoscopeObj[currentSign];
+  return (
+    <HoroscopeContext.Provider value={{ sign, setCurrentSign }}>
+      {props.children}
+    </HoroscopeContext.Provider>
+  );
+};
+export default HoroscopeProvider;
